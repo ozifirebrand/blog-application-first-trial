d8b57159622e9d63874dd5851b3ea25be4a2ce57
Ozioma <oziomaokoroafor@gmail.com>
2021-10-30 15:37:23 +0100

    Blog application commits

--- src/main/java/services/UserServices.java
+++ src/main/java/services/UserServices.java
@@ -3,12 +3,11 @@ package services;
 import models.User;
 
 public interface UserServices {
-    public User user = new User();
 
-    public void inputText(String inputText);
+    void inputText(String inputText);
 
-    public String displayInputText();
+    String displayInputText();
 
-    public void deleteText();
+    void deleteText();
 }
 
--- src/main/java/services/UserServicesImpl.java
+++ src/main/java/services/UserServicesImpl.java
@@ -1,8 +1,12 @@
 package services;
 
+import lombok.Data;
 import models.User;
 
 public class UserServicesImpl implements UserServices {
+
+    private User user = new User();
+
     @Override
     public void inputText(String inputText) {
 
