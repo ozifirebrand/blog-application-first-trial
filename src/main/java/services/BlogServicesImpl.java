a9b274a39daf7a4bfa705b4f845d4296da3f6e76
Ozioma <oziomaokoroafor@gmail.com>
2021-10-30 15:57:00 +0100

    Refactoring Blog services implemetation

--- src/main/java/services/BlogServicesImpl.java
+++ src/main/java/services/BlogServicesImpl.java
@@ -1,13 +1,17 @@
 package services;
 
+import models.User;
+
 public class BlogServicesImpl implements BlogServices{
     @Override
     public void inputText(String inputText) {
-
+        User user = blog.getUser();
+        user.setInputText(inputText);
     }
 
     @Override
     public String displayInputText() {
-        return null;
+        User user = blog.getUser();
+        return user.getInputText();
     }
 }
