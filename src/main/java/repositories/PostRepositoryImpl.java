abb2fb8a696cb7e6831f22bcb340f57e3a883f3c
Ozioma <oziomaokoroafor@gmail.com>
2021-11-10 22:45:01 +0100

    Completed the post repository

--- src/main/java/repositories/PostRepositoryImpl.java
+++ src/main/java/repositories/PostRepositoryImpl.java
@@ -2,31 +2,36 @@ package repositories;
 
 import models.Post;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PostRepositoryImpl implements PostRepository{
+    private final Map<Integer, Post> postsDatabase = new HashMap<>();
     @Override
     public Post save(Post post) {
-        return null;
+        postsDatabase.put(post.getPostId(), post);
+        return post;
     }
 
     @Override
     public Post findByPostId(Integer id) {
-        return null;
+        return postsDatabase.get(id);
     }
 
     @Override
     public List<Post> findAll() {
-        return null;
+        return new ArrayList<>(postsDatabase.values());
     }
 
     @Override
     public void delete(Post post) {
-
+        postsDatabase.remove(post.getPostId());
     }
 
     @Override
     public void delete(Integer postId) {
-
+        postsDatabase.remove(postId);
     }
 }
--- src/test/java/repositories/PostRepositoryImplTest.java
+++ src/test/java/repositories/PostRepositoryImplTest.java
@@ -1,5 +1,6 @@
 package repositories;
 
+import models.Post;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,9 +8,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PostRepositoryImplTest {
-
+    PostRepository postRepository;
+    Post post;
     @BeforeEach
     void setUp() {
+        postRepository = new PostRepositoryImpl();
+        post = new Post();
     }
 
     @AfterEach
@@ -17,22 +21,49 @@ class PostRepositoryImplTest {
     }
 
     @Test
-    void save() {
+    void test_save() {
+        assertEquals(post, postRepository.save(post));
     }
 
     @Test
     void findByPostId() {
+        post.setPostId(1);
+        postRepository.save(post);
+        assertEquals(post, postRepository.findByPostId(post.getPostId()));
     }
 
     @Test
     void findAll() {
+        post.setPostId(1);
+        Post post1 = new Post();
+        post1.setPostId(2);
+        Post post2 = new Post();
+        post2.setPostId(3);
+        Post post3 = new Post();
+        post3.setPostId(4);
+        postRepository.save(post);
+        postRepository.save(post1);
+        postRepository.save(post2);
+        postRepository.save(post3);
+        assertEquals(4, postRepository.findAll().size());
     }
 
     @Test
     void delete() {
+        postRepository.save(post);
+        assertEquals(1, postRepository.findAll().size());
+        postRepository.delete(post);
+        assertEquals(0, postRepository.findAll().size());
+
     }
 
     @Test
     void testDelete() {
+        post.setPostId(1);
+        postRepository.save(post);
+        assertEquals(1, postRepository.findAll().size());
+        postRepository.delete(1);
+        assertEquals(0, postRepository.findAll().size());
+
     }
 }
