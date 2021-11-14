56e1a11e74b723b8c41deceaa9e19b758851cd94
Ozioma <oziomaokoroafor@gmail.com>
2021-11-14 16:12:47 +0100

    Blog application user services commit 2

---
+++ src/test/java/services/UserServicesImplTest.java
@@ -0,0 +1,34 @@
+package services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServicesImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void test_createUser() {
+    }
+
+    @Test
+    void test_findAllUsers() {
+    }
+
+    @Test
+    void test_deleteUser() {
+    }
+
+    @Test
+    void test_findUserById() {
+    }
+}
